62c72726d6aa2705e72934f2231e0fe128d3a390
spybdai <spybdai@gmail.com>
2018-12-03 23:19:17 +0800

    another version of insertion sort

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -10,3 +10,14 @@ def insertion_sort(l):
                 l[j-1], l[j] = l[j], l[j-1]
             else:
                 break
+
+
+def insertion_sort_less(l):
+    for i in range(1, len(l)):
+        temp = l[i]
+        for j in range(i, 0, -1):
+            if l[j-1] > l[j]:
+                l[j-1] = l[j]
+            else:
+                l[j] = temp
+                break
--- sort/test_insertion_sort.py
+++ sort/test_insertion_sort.py
@@ -5,14 +5,23 @@ sys.path.append('..')
 
 from utils.timer import timer
 from utils.check import check_sort_increase
-from sort.insertion_sort import insertion_sort
+from sort.insertion_sort import insertion_sort, insertion_sort_less
 
 
 def test_insertion_sort():
 
-    l = [random.randint(1, 1000) for _ in range(1000)]
-    timer(insertion_sort)(l)
-    print check_sort_increase(l)
+    l = [random.randint(1, 100) for _ in range(100)]
+    print l
+
+    l2 = l[:]
+    print l2
+    timer(insertion_sort)(l2)
+    print check_sort_increase(l2)
+
+    l3 = l[:]
+    print l3
+    timer(insertion_sort_less)(l3)
+    print check_sort_increase(l3)
 
 
 def test():
